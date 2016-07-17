77acd548ea10ac219b446309c61a553d18d862af
arpdz2 <arpdz2@umsl.edu>
2016-07-17 16:27:18 -0500

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    morgan       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    http = require('http').Server(app),
+    ejs = require('ejs');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
